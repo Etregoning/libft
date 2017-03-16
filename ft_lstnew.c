14ef60dacb86cad9ef7114a6cd902e65efce8858
Elliot Tregoning <etregoni@e1z2r7p26.42.us.org>
2017-03-15 19:28:37 -0700

    Updated lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -11,15 +11,28 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 t_list	*ft_lstnew(const void *content, size_t content_size)
 {
 	t_list *res;
 
-	if (!content || !content_size)
+	res = (t_list*)malloc(sizeof(t_list));
+	if (!res)
 		return (NULL);
-	res = malloc(content_size);
-	res->content = (void *)content;
+	res->content = malloc(content_size);
+	res->content_size =(size_t)malloc(content_size);
+	res->next = (struct s_list *)malloc(sizeof(t_list));
+	if (!content)
+	{
+		res->content = NULL;
+		res->content_size = 0;
+		res->next = NULL;
+		return (res);
+	}
+	res->content = ft_memmove(res->content, content, content_size);
+	res->content_size = content_size;
 	res->next = NULL;
 	return (res);
 }
+
